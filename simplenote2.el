0994e21ae005b4c0a20e74d7648f670b452179ae
alpha22jp <alpha22jp@gmail.com>
2015-02-21 23:39:31 +0900

    Modify: modify commentary section in the header

--- simplenote2.el
+++ simplenote2.el
@@ -26,12 +26,14 @@
 
 ;;; Commentary:
 
-;; `simplenote2.el' is a new version of `simplenote.el'
-;; which assists the interaction with [Simplenote](http://simplenoteapp.com/).
-;; The major improvement points from the original version are
+;; This is a new version of simplenote.el which assists the interaction with
+;; Simplenote (http://app.simplenote.com/). The major improvement points from
+;; the original version are
 ;; 
-;; * Use of Simplenote API ver.2 to interact with server which can provide the support of tags, automatic merge of notes, and some other features.
-;; * Asynchronous and concurrent access to server as far as possible which brings faster sync of notes and non-blocking UI.
+;; * Use of Simplenote API ver.2 to interact with server which can provide the
+;;   support of tags, automatic merge of notes, and some other features.
+;; * Asynchronous and concurrent access to server which brings faster sync of
+;;   notes and non-blocking UI.
 
 ;;; Code:
 
